26e19ced5c950f38703d72876ab805da4116193c
jzy-xidian <790391139@qq.com>
2020-09-07 14:12:11 +0800

    消费者

--- src/com/hello/person/consumer.java
+++ src/com/hello/person/consumer.java
@@ -1,5 +1,20 @@
 package com.hello.person;
 
 public class consumer {
-    private int a;
+
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0]=1;
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        int[] b = a;
+        b[1] = 4;
+        for(int i=0;i<b.length;i++){
+            System.out.println(b[i]);
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
 }
